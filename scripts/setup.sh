49439327b5264abc8fe1c24203598bd91ff3abdb
祝云篪 <230218818@seu.edu.cn>
2022-08-30 07:57:50 +0800

    Initial commit

---
+++ scripts/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ ! "$(command -v docker)" ]; then
+  echo "Error: please install docker."
+  exit 1
+fi
+
+if [ ! "$(command -v wget)" ]; then
+  echo "Error: please install wget."
+  exit 1
+fi
+
+if [ ! "$(command -v curl)" ]; then
+  echo "Error: please install curl."
+  exit 1
+fi
+
+if [ ! "$(command -v pip)" ]; then
+  echo "Error: please install python3-pip."
+  exit 1
+fi
+
+pip install docker-compose
+
+if [ ! -f "app/php/maxit.tar.gz" ]; then
+	wget https://figshare.com/ndownloader/files/36918121 -O app/php/maxit.tar.gz;
+fi
+
+docker-compose up -d
