973160f3c467443b7244b80b7650a1f34c611ea8
祝云篪 <230218818@seu.edu.cn>
2022-08-30 17:44:35 +0800

    2022-8-30 17:44

--- web/api/pdb2alphacif/index.php
+++ web/api/pdb2alphacif/index.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+JSON request:
+    {
+        "name": protein name
+        "data": base64-encoded PDB text
+    }
+Response: CIF text
+*/
+
 $PDB_Raw_Data = file_get_contents("php://input");
 $Protein_Name = json_decode($PDB_Raw_Data,true)["name"];
 $PDB = base64_decode(json_decode($PDB_Raw_Data,true)["data"]);
