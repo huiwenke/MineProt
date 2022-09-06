b172fbed25ded40379fd7618fefa6d81cb332703
PC-DOS <suicunexp@outlook.com>
2022-09-06 12:45:41 +0800

    Comment RandomStr.py

--- scripts/RandomStr.py
+++ scripts/RandomStr.py
@@ -4,5 +4,10 @@ import random
 import string
 
 def RandomStr(num):
+    """
+    Generate a random string.
+    :param num: Seed number, should be 0 or positive, int
+    :return: MD5 encoded random string, str
+    """
     salt = ''.join(random.sample(string.ascii_letters+string.digits, num))+str(time.time())
     return hashlib.md5(salt.encode("utf-8")).hexdigest()
