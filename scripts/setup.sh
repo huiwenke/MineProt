23fa85e8ad189f67f281dff0ed4358b94b50c57e
祝云篪 <230218818@seu.edu.cn>
2022-09-09 10:13:18 +0800

    setup update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,33 @@
 #!/bin/bash -e
 
+MP_PORT=80
+MP_DATA=data
+
+while [ -n "$1" ]
+do
+    case "$1" in
+        -d)
+            MP_DATA=$2
+            shift
+        ;;
+        -p)
+            MP_PORT=$2
+            shift
+        ;;
+        -h)
+            echo "Usage: setup.sh [options...]"
+            echo "-d <dir>        MineProt data directory (default: ./data)"
+            echo "-p <port>       MineProt port (default: 80)"
+            exit
+        ;;
+        *)
+            echo "Invalid paramter. try '-h' for more information."
+            exit
+        ;;
+    esac
+    shift
+done
+
 if [ ! "$(command -v docker)" ]; then
   echo "Error: Please install docker."
   exit 1
@@ -21,12 +49,16 @@ if [ ! -f "app/php/maxit.tar.gz" ]; then
 	wget https://figshare.com/ndownloader/files/36918121 -O app/php/maxit.tar.gz;
 fi
 
-if [ ! -d "data" ]; then
-	mkdir data
+if [ ! -d $MP_DATA ]; then
+	mkdir $MP_DATA
 fi
 
-if [ ! -d "data/es" ]; then
-	cp -r app/elasticsearch/init data/es
+if [ ! -d $MP_DATA/es ]; then
+	cp -r app/elasticsearch/init $MP_DATA/es
 fi
 
+tee .env <<EOF
+MP_PORT=$MP_PORT
+MP_DATA=$MP_DATA
+EOF
 docker-compose up -d
