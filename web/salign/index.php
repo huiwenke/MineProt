6193b49bb4ebfd0787f50eb8ab374d1ef5a68886
huiwenke <kuailexuetong@sina.com>
2022-10-20 15:11:49 +0800

    index.php init

---
+++ web/salign/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MineProt</title>
+    <link rel="shortcut icon" type="image/png" href="../assets/img/logo.png">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+</head>
+
+<body>
+    <?php
+    include "header.html";
+    include "../api.php";
+    $DATA_REPOS = array_diff(scandir(getenv("MP_REPO_PATH")), array('.', '..'));
+    foreach ($DATA_REPOS as $Data_Repo_k => $Data_Repo) {
+        if (!get_api_repo($Data_Repo)) {
+            unset($DATA_REPOS[$Data_Repo_k]);
+        }
+    }
+    ?>
+    <main style="min-height: 100vh;">
+        <section class="main_salign">
+            <div style="display: block; width: 100%;">
+                <form>
+                    <br><br>
+                    <label class="wrapper">
+                        <textarea name="structure" class="form-control-header" style="height: 25vh;" placeholder="Paste query structure here..."></textarea>
+                    </label>
+                    <br>
+                    <h2 class="h2_aside">Protein Repositories</h2>
+                    <HR color=#21262d SIZE=1.5>
+                    <?php
+                    include "../display.php";
+                    foreach ($DATA_REPOS as $Data_Repo) {
+                        form_repo($Data_Repo);
+                    }
+                    ?>
+                    <br>
+                    <div style="display: flex;">
+                        <div style="width: 15%; color: white;">
+                            <strong>Key words</strong>
+                        </div>
+                        <div style="width: 85%; color: white;">
+                            <input name="search" class="input-import" placeholder="🔍︎ Enter keywords...">
+                        </div>
+                    </div>
+                    <br>
+                    <div style="display: flex;">
+                        <div style="width: 15%; color: white;">
+                            <strong>RMSD</strong>
+                        </div>
+                        <div style="width: 25%; color: white;">
+                            <input name="rmsd" class="input-import" value=8>
+                        </div>
+                        <div style="width: 10%;">
+                        </div>
+                        <div style="width: 50%;">
+                            <button class="btn" type="submit" style="width: 100%;"><strong>Search</strong></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
