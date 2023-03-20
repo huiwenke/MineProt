979ca60e16b9cea40c3334b211dcf7976075d88b
祝云篪 <kuailexuetong@sina.com>
2023-03-20 16:50:21 +0800

    toolkit support 3d-beacons

--- toolkit/scripts/annotate.py
+++ toolkit/scripts/annotate.py
@@ -1,5 +1,9 @@
 import requests
 import json
+from Bio.Align import PairwiseAligner
+from Bio.Align import substitution_matrices
+from Bio.Seq import Seq
+
 
 def GetUniProtKB(accession):
     """
@@ -7,8 +11,8 @@ def GetUniProtKB(accession):
     :param accession: Candidate UniProt accession, str
     :return: JSON response from UniProt API, obj
     """
-    headers = {'Accept': 'application/json'}
-    request_url = "https://www.ebi.ac.uk/proteins/api/proteins/"+accession
+    headers = {"Accept": "application/json"}
+    request_url = "https://www.ebi.ac.uk/proteins/api/proteins/" + accession
     retry = 0
     while retry < 3:
         try:
@@ -17,14 +21,17 @@ def GetUniProtKB(accession):
         except:
             retry += 1
 
+
 def GetUniParc(upi):
     """
     Get annotations from UniProt API (UniParc service).
     :param upi: Candidate UniParc identifier, str
     :return: JSON response from UniProt API, obj
     """
-    headers = {'Accept': 'application/json'}
-    request_url = "https://www.ebi.ac.uk/proteins/api/uniparc/upi/"+upi+"?rfDdtype=RefSeq"
+    headers = {"Accept": "application/json"}
+    request_url = (
+        "https://www.ebi.ac.uk/proteins/api/uniparc/upi/" + upi + "?rfDdtype=RefSeq"
+    )
     retry = 0
     while retry < 3:
         try:
@@ -32,6 +39,23 @@ def GetUniParc(upi):
             return response
         except:
             retry += 1
+
+
+def FixAccession(identifier):
+    """
+    Fix A3M identifier to legal accession
+    :param identifier: Input identifier, str
+    :return: Fixed accession, str
+    """
+    accession = identifier.split()[0]
+    if accession[0] == ">":
+        accession = accession[1:]
+    if accession[0:3] in ["sp|", "tr|"]:
+        accession = accession.split("|")[1]
+    if "UniRef100_" in accession:
+        accession = accession.split("_")[1]
+    return accession
+
 
 def UniProt2MineProt(identifier_list, max_msa):
     """
@@ -40,31 +64,25 @@ def UniProt2MineProt(identifier_list, max_msa):
     :param max_msa: Max number of candidate msas to use for annotation, int
     :return: Formatted annotations, dict
     """
-    result_json = {
-        "homolog": "",
-        "database": "",
-        "description": []
-    }
+    result_json = {"homolog": "", "database": "", "description": []}
     identifier_num = max_msa
     for identifier in identifier_list:
         identifier_num -= 1
-        if identifier_num<0:
+        if identifier_num < 0:
             break
-        accession = identifier.split()[0]
-        if accession[0]=='>':
-            accession = accession[1:]
-        if accession[0:3] in ["sp|", "tr|"]:
-            accession = accession.split('|')[1]
-        if "UniRef100_" in accession:
-            accession = accession.split('_')[1]
+        accession = FixAccession(identifier)
         response = GetUniProtKB(accession)
         if response.status_code == 200:
             response_json = json.loads(response.text)
             result_json["homolog"] = response_json["accession"]
             try:
-                result_json["description"].append(response_json["protein"]["submittedName"][0]["fullName"]["value"])
+                result_json["description"].append(
+                    response_json["protein"]["submittedName"][0]["fullName"]["value"]
+                )
             except:
-                result_json["description"].append(response_json["protein"]["recommendedName"]["fullName"]["value"])
+                result_json["description"].append(
+                    response_json["protein"]["recommendedName"]["fullName"]["value"]
+                )
             result_json["database"] = "uniprotkb"
             result_json["description"].append(response_json["accession"])
             for dbReference in response_json["dbReferences"]:
@@ -73,7 +91,9 @@ def UniProt2MineProt(identifier_list, max_msa):
                     result_json["description"].append(dbReference["properties"]["term"])
                 if dbReference["type"] == "InterPro":
                     result_json["description"].append(dbReference["id"])
-                    result_json["description"].append(dbReference["properties"]["entry name"])
+                    result_json["description"].append(
+                        dbReference["properties"]["entry name"]
+                    )
             break
         response = GetUniParc(accession)
         if response.status_code == 200:
@@ -89,9 +109,74 @@ def UniProt2MineProt(identifier_list, max_msa):
                 result_json["description"].append(response_json["accession"])
                 for signatureSequenceMatch in response_json["signatureSequenceMatch"]:
                     if "ipr" in signatureSequenceMatch:
-                        result_json["description"].append(signatureSequenceMatch["ipr"]["id"])
-                        result_json["description"].append(signatureSequenceMatch["ipr"]["name"])
+                        result_json["description"].append(
+                            signatureSequenceMatch["ipr"]["id"]
+                        )
+                        result_json["description"].append(
+                            signatureSequenceMatch["ipr"]["name"]
+                        )
                 break
             except:
                 continue
+    return result_json
+
+
+def Align(seq1, seq2):
+    aligner = PairwiseAligner()
+    aligner.mode = "local"
+    aligner.substitution_matrix = substitution_matrices.load("BLOSUM62")
+    aligner.gap_score = -10
+    aligner.extend_gap_score = -0.5
+    alignments = aligner.align(seq1, seq2)
+    alignment = alignments[0]
+    start = alignment.aligned[0][0][0] + 1
+    end = alignment.aligned[0][-1][1] + 1
+    sequenceIdentity = str(alignment).count("|") / alignment.shape[1] * 100
+    coverage = (end - start + 1) / len(seq1)
+    return {
+        "start": int(start),
+        "end": int(end),
+        "sequenceIdentity": float(sequenceIdentity),
+        "coverage": float(coverage),
+        "alignment": alignment,
+    }
+
+
+def Make3bMeta(seq, confidenceAvgLocalScore, createdDate, identifier_list, max_msa):
+    """
+    Generate metadata for 3D-Beacons client
+    :param seq: Sequence, str
+    :param identifier_list: Candidate UniProt homolog list, list
+    :param max_msa: Max number of candidate msas to use for annotation, int
+    :return: Metadata, json
+    """
+    result_json = {
+        "mappingAccession": "",
+        "mappingAccessionType": "uniprot",
+        "start": 0,
+        "end": 0,
+        "modelCategory": "TEMPLATE-BASED",
+        "modelType": "single",
+        "confidenceType": "pLDDT",
+        "confidenceAvgLocalScore": confidenceAvgLocalScore,
+        "createdDate": createdDate,
+        "sequenceIdentity": 0.0,
+        "coverage": 0.0,
+    }
+    identifier_num = max_msa
+    for identifier in identifier_list:
+        identifier_num -= 1
+        if identifier_num < 0:
+            break
+        accession = FixAccession(identifier)
+        response = GetUniProtKB(accession)
+        if response.status_code == 200:
+            response_json = json.loads(response.text)
+            align_json = Align(Seq(response_json["sequence"]["sequence"]), Seq(seq))
+            result_json["mappingAccession"] = accession
+            result_json["start"] = align_json["start"]
+            result_json["end"] = align_json["end"]
+            result_json["sequenceIdentity"] = align_json["sequenceIdentity"]
+            result_json["coverage"] = align_json["coverage"]
+            break
     return result_json
---
+++ toolkit/scripts/pre4beacon.py
@@ -0,0 +1,72 @@
+import argparse
+import os
+import time
+import sys
+import shutil
+import json
+import threading
+sys.path.append(os.path.abspath(os.path.dirname(sys.argv[0])))
+from annotate import Make3bMeta
+
+# List arguments
+parser = argparse.ArgumentParser(description='Prepare metadata for 3D-Beacons client.')
+parser.add_argument('-i', type=str, help="Path to your MineProt repo. THIS ARGUMENT IS MANDATORY.")
+parser.add_argument('-o', type=str, help="Path to your data directory for 3D-Beacons. THIS ARGUMENT IS MANDATORY.")
+parser.add_argument('-t', type=int, default=1, help="Threads to use.")
+parser.add_argument('--max-msa', dest="max_msa", type=int, default=30, help="Max number of msas to use for mapping.")
+
+# Now parse user-given arguments
+args = parser.parse_args()
+Max_MSA = args.max_msa
+# Mandatory argument -i, configures input directory
+InputDir = args.i
+print("Will use proteins stored in "+InputDir)
+# Mandatory argument -o, configures output directory
+OutputDir = args.o
+if not os.path.exists(OutputDir):
+    os.makedirs(OutputDir)
+DirPDB = os.path.join(OutputDir, "pdb")
+if not os.path.exists(DirPDB):
+    os.makedirs(DirPDB)
+DirMetadata = os.path.join(OutputDir, "metadata")
+if not os.path.exists(DirMetadata):
+    os.makedirs(DirMetadata)
+DirCIF = os.path.join(OutputDir, "cif")
+if not os.path.exists(DirCIF):
+    os.makedirs(DirCIF)
+DirIndex = os.path.join(OutputDir, "index")
+if not os.path.exists(DirIndex):
+    os.makedirs(DirIndex)
+
+def worker(semaphore, file_name):
+    file_path = os.path.join(InputDir, file_name)
+    createdDate = time.strftime("%Y-%m-%d", time.localtime(os.path.getctime(file_path)))
+    output_path = os.path.join(DirPDB, file_name)
+    shutil.copyfile(file_path, output_path)
+    protein = os.path.splitext(file_name)[0]
+    with open(os.path.join(InputDir, protein)+".json", 'r') as fin_json:
+        score_plddt = json.load(fin_json)["plddt"]
+        confidenceAvgLocalScore = sum(score_plddt)/len(score_plddt)
+    with open(os.path.join(InputDir, protein)+".a3m", 'r') as f_a3m, open(os.path.join(DirMetadata, protein)+".json", 'w') as fout_json:
+        lines = f_a3m.readlines()
+        seq = lines[2][:-1]
+        meta_json = Make3bMeta(seq, confidenceAvgLocalScore, createdDate, lines[3::2], Max_MSA)
+        json.dump(meta_json, fout_json)
+    semaphore.release()
+
+semaphore = threading.Semaphore(args.t)
+threads = []
+# Start importing
+print("Prepare metadata for 3D-Beacons...")
+# Enumerate all files in InputDir
+for file_name in os.listdir(InputDir):
+    if os.path.splitext(file_name)[-1] == ".pdb":
+        semaphore.acquire()
+        t = threading.Thread(target=worker, args=(semaphore, file_name))
+        threads.append(t)
+        t.start()
+for t in threads:
+    t.join()
+
+# All done
+print("Done.")
