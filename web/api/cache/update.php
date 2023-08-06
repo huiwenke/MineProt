96e14a5f4eb03303256916ab602894ceebeaba83
祝云篪 <213170096@seu.edu.cn>
2023-08-06 14:26:19 +0800

    cache/update bugfix

--- web/api/cache/update.php
+++ web/api/cache/update.php
@@ -5,14 +5,14 @@ $Cache_Dirs = scandir(sys_get_temp_dir());
 foreach ($Cache_Dirs as $Cache_Dir) {
     if (strpos($Cache_Dir, "MP_SALIGN") === 0) {
         $Cache_Path = sys_get_temp_dir() . '/' . $Cache_Dir;
-        $Cache_Time = time()-filemtime($Cache_Path);
-        if ($Cache_Time>86400){
+        $Cache_Time = time() - filemtime($Cache_Path);
+        if ($Cache_Time > 86400) {
             shell_exec("rm -rf $Cache_Path");
         }
     }
 }
 
 foreach ($DATA_REPOS as $Data_Repo) {
-    shell_exec("curl http://MineNginx/browse.php?repo=$Data_Repo");
-    shell_exec("curl -X POST -F 'sort=plddt' http://MineNginx/browse.php?repo=$Data_Repo");
+    shell_exec("curl " . getenv("MP_LOCALHOST") . "/browse.php?repo=$Data_Repo");
+    shell_exec("curl -X POST -F 'sort=plddt' " . getenv("MP_LOCALHOST") . "/browse.php?repo=$Data_Repo");
 }
